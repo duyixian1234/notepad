4c07e0a839508089c7e7bcbb64ff390224848e8d
Yixian Du <duyixian1234@qq.com>
2023-12-29 21:35:33 +0800

    use redis

--- api/index.py
+++ api/index.py
@@ -1,15 +1,71 @@
+import os
 import random
 from base64 import urlsafe_b64encode
 
-from fastapi import FastAPI, Request
+from fastapi import Depends, FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from html_dsl.common import *
 from html_dsl.elements import BaseHtmlElement
+from redis.asyncio import Redis
+
+
+def str_without_blanks(element: BaseHtmlElement):
+    blank = ""
+    attrs = (
+        " {}".format(
+            " ".join(
+                f'{key.replace("_", "-")}="{str(element.attrs[key])}"'
+                for key in element.attrs
+            )
+        )
+        if element.attrs
+        else ""
+    )
+    children = "\n".join(
+        str_without_blanks(child)
+        if isinstance(child, BaseHtmlElement)
+        else blank + str(child)
+        for child in element.children
+    )
+    if element.single:
+        return "{blank}<{name}{attrs}>".format(
+            blank=blank, name=element.name, attrs=attrs
+        )
+    if element.no_content:
+        return "{blank}<{name}{attrs}/>".format(
+            blank=blank, name=element.name, attrs=attrs
+        )
+    return "{blank}<{name}{attrs}>\n{children}\n{blank}</{name}>".format(
+        blank=blank, name=element.name, attrs=attrs, children=children
+    )
 
 
 def render(element: BaseHtmlElement):
-    return HTMLResponse(str(element))
+    return HTMLResponse(str_without_blanks(element))
 
+
+def get_redis():
+    yield Redis(
+        host=os.getenv("REDIS_URL"),
+        port=37513,
+        password=os.getenv("REDIS_PASSWORD"),
+        ssl=True,
+    )
+
+
+script = """function save() {
+  fetch(location.toString(), {
+    method: "POST",
+    body: document.getElementById("editor").value,
+  });
+}"""
+
+head = HEAD[
+    META['charset="utf-8"'],
+    META['name="viewport" content="width=device-width, initial-scale=1"'],
+    TITLE["Notepad"],
+    SCRIPT[script],
+]
 
 app = FastAPI()
 
@@ -21,31 +77,26 @@ async def root():
 
 
 @app.get("/{id}")
-async def note(id: str):
+async def note(id: str, redis: Redis = Depends(get_redis)):
+    content = (raw := await redis.get(f"note:{id}")) and raw.decode()
+
     return render(
         HTML(lang="en")[
-            HEAD[
-                META['charset="utf-8"'],
-                META['name="viewport" content="width=device-width, initial-scale=1"'],
-                TITLE["Notepad"],
-                SCRIPT[
-                    """
-setInterval(() => {fetch(location.toString(),{method:'POST',body:document.getElementById("editor").value})},3000)
-"""
-                ],
-            ],
+            head,
             BODY[
+                BUTTON(onClick="save()")["Save"],
                 TEXTAREA(
                     id="editor",
                     placeholder="input text here",
                     style="width:100%;height:100%;font-size:20px;",
-                )
+                )[content or ""],
             ],
         ]
     )
 
 
 @app.post("/{id}")
-async def note_post(id: str, request: Request):
-    print(await request.body())
+async def note_post(id: str, request: Request, redis: Redis = Depends(get_redis)):
+    content = (await request.body()).decode()
+    await redis.set(f"note:{id}", content)
     return ""
